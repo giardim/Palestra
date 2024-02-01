d84230832b9a54b9a9497ae2a8d9d28427dbee36
Michael Giardina <mgiardina02@gmail.com>
2024-02-01 11:46:45 -0500

    Fixed fragments to work properly

--- Palestra-Android/app/src/main/java/com/example/palestra/MainActivity.java
+++ Palestra-Android/app/src/main/java/com/example/palestra/MainActivity.java
@@ -5,35 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.FrameLayout;
-
+import android.widget.Toast;;
 import com.example.palestra.databinding.ActivityMainBinding;
 import com.google.android.material.navigation.NavigationBarMenu;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
-    FrameLayout frameLayout;
-    NavigationBarMenu navbar;
-
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new profileFragment());
 
-        frameLayout = (FrameLayout) findViewById(R.id.homeFragment);
+        binding.navBar.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == (R.id.profileIcon)){
+                replaceFragment(new profileFragment());
+            }
+            else if (item.getItemId() == (R.id.addFriendIcon)){
+                replaceFragment(new searchFragment());
+            }
+
+            return true;
+        });
 
 
     }
 
     private void replaceFragment(Fragment fragment){
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.homeFragment, fragment);
-        transaction.commit();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.homeFragment, fragment);
+        ft.commit();
     }
 
     @Override
